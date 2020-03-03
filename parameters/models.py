51262483549f65e8f3820b52b40092c115fc1ec5
silver_klover <faiz.kadir.ismail@gmail.com>
2020-03-03 20:29:05 +0800

    Version 01 - update migrations file

--- parameters/models.py
+++ parameters/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class ActivateBoolean(models.Model):
-    activation_type = models.TextField(unique=True, blank=False)
+    activation_type = models.CharField(unique=True, blank=False, max_length=15)
     activate = models.BooleanField(default=False)
 
     def __str__(self):
@@ -10,14 +10,14 @@ class ActivateBoolean(models.Model):
 
 
 class EmailType(models.Model):
-    email_type = models.TextField(unique=True, blank=False, max_length=15)
+    email_type = models.CharField(unique=True, blank=False, max_length=15)
 
     def __str__(self):
         return f'{self.email_type}'
 
 
 class EmailList(models.Model):
-    email_type = models.ForeignKey(EmailType, on_delete=models.CASCADE)
+    email_type = models.ForeignKey(EmailType, on_delete=models.CASCADE, max_length=15)
     email = models.EmailField(null=False)
 
     def __str__(self):
